51a0911a554e0eac34de28c798e944a31226e398
LionelMN <46958498+LionelMN@users.noreply.github.com>
2022-09-14 11:41:37 +0100

    Added ';' and title

--- tabla.php
+++ tabla.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio 1 - Tabla</title>
 </head>
 <body>
     <?php
@@ -15,7 +15,7 @@
                     <td>'.date('H:i:s:a').'</td>
                 </tr>
             </table>
-            '
+            ';
     ?>
 </body>
 </html>
