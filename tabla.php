595063623bfc5820d0d108052514fb3a1221027e
LionelMN <46958498+LionelMN@users.noreply.github.com>
2022-09-14 11:33:03 +0100

    Hecho ejercicio 1

---
+++ tabla.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '
+            <table border=1>
+                <tr>
+                    <td>'.date('r').'</td>
+                    <td>'.date('H:i:s:a').'</td>
+                </tr>
+            </table>
+            '
+    ?>
+</body>
+</html>
